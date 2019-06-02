cf79687ddd8cfb6b921721da884fcd41f6114acf
Aziz Ali <aziz@ilovecoding.org>
2019-06-02 14:00:53 -0500

    Add button - event listener

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// 1. User clicks on the button
+// 2. We capture the text in the input field
+// 3. display that text on the screen
+// 4. When delete is clicked, delete the item
+
 const addButton = document.querySelector('#addButton');
+const itemToAdd = document.querySelector('#itemToAdd');
 
 addButton.addEventListener('click', function(){
-
+  console.log('I was clicked', itemToAdd.value);
 });
